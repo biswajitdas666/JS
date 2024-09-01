478bcd2a5a6dd5b2cce7019328d07ac8a878880c
BISWAJIT DAS <138815139+biswajitdas666@users.noreply.github.com>
2024-09-01 13:17:30 +0000

    kjbkxcbjhb

---
+++ .devcontainer/01_basics/03_conversionoperation.js
@@ -0,0 +1,15 @@
+let score=33
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber=Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+let isLoggedIn="hitesh"
+let booleanIsLoggedIn=Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+let someNumber=33
+let stringNumber=String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
